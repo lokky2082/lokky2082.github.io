e6164ec51e2af90f6344f4a4e61e71edef654f33
lokky2082 <lokky2082@gmail.com>
2017-04-24 10:00:17 +0300

    firstPage

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+(function() {
+  'use strict';
+
+
+})();
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbIihmdW5jdGlvbigpIHtcclxuICAndXNlIHN0cmljdCc7XHJcblxyXG5cclxufSkoKTsiXX0=
